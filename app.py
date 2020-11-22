9fde1277cd658a13a821ac8ca6a501a414ffb72a
micale24 <kaihall.usaf@gmail.com>
2020-11-22 15:11:31 -0500

    stuff

--- app.py
+++ app.py
@@ -19,38 +19,14 @@ def home():
     return render_template("index.html")
 
 
-# Query the database and send the jsonified results
 @app.route("/graph")
 def graph():
 
     return render_template("graph.html")
 
-
-@app.route("/api/pals")
-def pals():
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
-
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
-
-    pet_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 50,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
-
-    return jsonify(pet_data)
+@app.route("/map")
+def map():
+    return render_template("map.html")
 
 
 if __name__ == "__main__":
---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibWljYWxlMjQiLCJhIjoiY2toNWgyZW5hMGxteDJxb3ZiNmZ5OXZ4ZiJ9.C1FvlwQwq9wOlKsS6KwN2g";
