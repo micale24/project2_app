7220d579cbd274d3a7608b43dcf502969121e6c7
micale24 <kaihall.usaf@gmail.com>
2020-11-23 09:58:18 -0500

    finished the map page

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,61 @@
+// // Creating map object
+// var myMap = L.map("map", {
+//   center: [39.15, -77.24],
+//   zoom: 11
+// });
+
+// // Adding tile layer to the map
+// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   maxZoom: 18,
+//   zoomOffset: -1,
+//   id: "mapbox/streets-v11",
+//   accessToken: API_KEY
+// }).addTo(myMap);
+
+// // Store API query variables
+// var baseURL = "https://data.montgomerycountymd.gov/resource/bhju-22kf.json";
+// // var date = "$where=created_date between'2016-01-01T00:00:00' and '2017-01-01T00:00:00'";
+// // var complaint = "&complaint_type=Rodent";
+// // var limit = "&$limit=10000";
+
+// // Assemble API query URL
+// var url = baseURL; 
+// // + date + complaint + limit;
+
+// // Grab the data with d3
+// d3.json(url, function(response) {
+
+//   // Create a new marker cluster group
+//   var markers = L.markerClusterGroup();
+
+//   // Loop through data
+//   for (var i = 0; i < response.length; i++) {
+
+//     // Set the data location property to a variable
+//     // var location = response[i].data;
+//     var location = response["geolocation"];
+//     console.log(location);
+//     // Check for location property
+//     if (location) {
+
+//       // Add a new marker to the cluster group and bind a pop-up
+//       markers.addLayer(L.marker([location.latitude, location.longitude)
+//         .bindPopup(response[i]).agency_name);
+//     }
+
+//   }
+
+//   // Add our marker cluster layer to the map
+//   myMap.addLayer(markers);
+
+// });
+
 // Creating map object
 var myMap = L.map("map", {
-  center: [39.1547, 77.2405],
+  // center: [40.7, -73.95],
+  center: [39.15, -77.24],
   zoom: 11
 });
 
@@ -21,7 +76,7 @@ var baseURL = "https://data.montgomerycountymd.gov/resource/bhju-22kf.json";
 // var limit = "&$limit=10000";
 
 // Assemble API query URL
-var url = baseURL; 
+var url = baseURL 
 // + date + complaint + limit;
 
 // Grab the data with d3
@@ -34,17 +89,19 @@ d3.json(url, function(response) {
   for (var i = 0; i < response.length; i++) {
 
     // Set the data location property to a variable
-    // var location = response[i].data;
-    var location = response["geolocation"];
-
+    
+    var geolocation = response[i];
+    
     // Check for location property
     if (location) {
 
       // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([location.latitude, location.longitude)
-        .bindPopup(response[i]).agency_name);
-    }
+      
+      markers.addLayer(L.marker([geolocation.latitude, geolocation.longitude])
+      .bindPopup(response[i].acrs_report_type));
 
+    }
+      
   }
 
   // Add our marker cluster layer to the map
@@ -53,5 +110,5 @@ d3.json(url, function(response) {
 });
 
 
-ploc = parkingData["parkeerlocaties"][x]["parkeerlocatie"]["Locatie"]["coordinates"];
-console.log(ploc)
+
+
