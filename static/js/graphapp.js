51e71356c42df4068a371d7ae7fa7fb6bf53031a
micale24 <kaihall.usaf@gmail.com>
2020-11-24 16:25:52 -0500

    sfsf

---
+++ static/js/graphapp.js
@@ -0,0 +1,76 @@
+// Define SVG area dimensions
+var svgWidth = 2000;
+var svgHeight = 900;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 100,
+  right: 100,
+  bottom: 100,
+  left: 100
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3.select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from hours-of-tv-watched.csv
+d3.csv("agency_vis_1.csv").then(function(tvData) {
+
+  console.log(tvData);
+
+  // Cast the hours value to a number for each piece of tvData
+  tvData.forEach(function(d) {
+    d.ReportCount = +d.ReportCount;
+  });
+
+  // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
+  var xBandScale = d3.scaleBand()
+    .domain(tvData.map(d => d.Agency))
+    .range([0, chartWidth])
+    .padding(0.1);
+
+  // Create a linear scale for the vertical axis.
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(tvData, d => d.ReportCount)])
+    .range([chartHeight, 0]);
+
+  // Create two new functions passing our scales in as arguments
+  // These will be used to create the chart's axes
+  var bottomAxis = d3.axisBottom(xBandScale);
+  var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
+
+  // Append two SVG group elements to the chartGroup area,
+  // and create the bottom and left axes inside of them
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
+
+  // Create one SVG rectangle per piece of tvData
+  // Use the linear and band scales to position each rectangle within the chart
+  chartGroup.selectAll(".bar")
+    .data(tvData)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", d => xBandScale(d.Agency))
+    .attr("y", d => yLinearScale(d.ReportCount))
+    .attr("width", xBandScale.bandwidth())
+    .attr("height", d => chartHeight - yLinearScale(d.ReportCount));
+
+}).catch(function(error) {
+  console.log(error);
+});
